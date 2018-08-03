522171b5f7d7cc49c207be47c1dfb319b113e069
bhargav145 <bhargav145@local>
2018-08-03 20:58:35 +0000

    my lab1 work

--- lab1/text_recognizer/networks/mlp.py
+++ lab1/text_recognizer/networks/mlp.py
@@ -1,27 +1,38 @@
 from typing import Tuple
+import numpy as np
 
 from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Dense, Dropout, Flatten,Conv2D,Reshape
+from tensorflow.keras import layers
 
 
 def mlp(input_shape: Tuple[int, ...],
         output_shape: Tuple[int, ...],
         layer_size: int=128,
-        dropout_amount: float=0.2,
+        dropout_amount: float=0.3,
         num_layers: int=3) -> Model:
     """
     Simple multi-layer perceptron: just fully-connected layers with dropout between them, with softmax predictions.
     Creates num_layers layers.
     """
     num_classes = output_shape[0]
+    #print("input_shape",input_shape)
+    input_shape = np.array(input_shape)
+    #input_shape = np.expand_dims(input_shape, axis=0)
 
     model = Sequential()
     # Don't forget to pass input_shape to the first layer of the model
     ##### Your code below (Lab 1)
-    model.add(Flatten(input_shape=input_shape))
-    for i in range(num_layers):
-        model.add(Dense(layer_size,activation='relu'))
-        model.add(Dropout(dropout_amount))
+    model.add(Reshape((28, 28,1), input_shape=(28,28)))
+    model.add(Conv2D(32,(3,3),activation='relu',input_shape = input_shape))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(Conv2D(64,(3,3),activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))    
+    model.add(Conv2D(128,(3,3),activation='relu'))
+    model.add(Flatten())
+    model.add(Dropout(dropout_amount))
+    model.add(Dense(512,activation='relu'))
+    model.add(Dropout(dropout_amount))
     model.add(Dense(num_classes,activation='softmax'))
     ##### Your code above (Lab 1)
 
